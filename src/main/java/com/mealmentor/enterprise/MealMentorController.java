e91e83c51a6f7cce1f7704e11129d36bd8c10e72
johnadkerson <sinewizard73@gmail.com>
2021-10-01 14:45:29 -0400

    Updated Controller to handle fetch(), delete(), create()

--- src/main/java/com/mealmentor/enterprise/MealMentorController.java
+++ src/main/java/com/mealmentor/enterprise/MealMentorController.java
@@ -1,13 +1,63 @@
 package com.mealmentor.enterprise;
 
+import com.mealmentor.enterprise.dto.MealItem;
+import com.mealmentor.enterprise.service.IMealPlanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class MealMentorController {
 
+    @Autowired
+    IMealPlanService mealPlanService;
+
     @RequestMapping("/")
     public String index() {
         return "start";
+    }
+
+    @GetMapping("/mealItem")
+    @ResponseBody
+    public List<MealItem> fetchAllMealItems(){
+
+        return mealPlanService.fetchAll();
+    }
+
+    @GetMapping("/mealItem/{id}/")
+    public ResponseEntity fetchMealItemById(@PathVariable("id") String id) {
+        MealItem foundMealItem = mealPlanService.fetchById(Integer.parseInt(id));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity(foundMealItem, headers, HttpStatus.OK);
+    }
+
+    @PostMapping(value="/mealItem", consumes="application/json", produces="application/json")
+    public MealItem createMealItem(@RequestBody MealItem mealItem) throws Exception {
+        MealItem newMealItem = null;
+        try{
+            newMealItem = mealPlanService.save(mealItem);
+        } catch (Exception e) {
+            //TODO ADD LOGGING
+
+        }
+        return newMealItem;
+    }
+
+    @DeleteMapping("/mealItem/{id}/")
+    public ResponseEntity deleteMealItem(@PathVariable("id") String id) {
+        try {
+            mealPlanService.delete(Integer.parseInt(id));
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 }
