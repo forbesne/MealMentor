fa758d96942a93d74d4f930e658b2b939afd2f00
ThapaArchana <thapaan@mail.uc.edu>
2021-10-01 17:53:57 -0400

    Added reference to thymeleaf and load Model

--- src/main/java/com/mealmentor/enterprise/MealMentorController.java
+++ src/main/java/com/mealmentor/enterprise/MealMentorController.java
@@ -1,7 +1,10 @@
 package com.mealmentor.enterprise;
 
+import com.mealmentor.enterprise.dto.Recipe;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MealMentorController {
@@ -10,4 +13,11 @@ public class MealMentorController {
     public String index() {
         return "start";
     }
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String read(Model model){
+        model.addAttribute("Recipe", new Recipe());
+        return "start";
+    }
+
 }
