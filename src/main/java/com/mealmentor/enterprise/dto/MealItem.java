78c0273e81901d6e1af472130f30e67745e053db
bentonjc <bentonjc@mail.uc.edu>
2021-10-29 23:36:13 -0400

    Changing Recipe recipe to Integer recipeId to make it easier to store and pass recipes

--- src/main/java/com/mealmentor/enterprise/dto/MealItem.java
+++ src/main/java/com/mealmentor/enterprise/dto/MealItem.java
@@ -6,7 +6,7 @@ public @Data
 class MealItem {
     private String mealtime;
     private String day;
-    private Recipe recipe;
+    private Integer recipeId;
 
     public String getMealItemId() {
         String mealItemId = "";
