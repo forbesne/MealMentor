9b1cfc98e223861758912f6e88db081c79fc1cd6
bentonjc <bentonjc@mail.uc.edu>
2021-10-30 15:53:30 -0400

    Update the MealItem.dto in order to store its information in our database. User meals will be saved and stored in the database.
    MealItems are being saved to the database with null values, working on connecting day's/meal time to update and save as well.

--- src/main/java/com/mealmentor/enterprise/dto/MealItem.java
+++ src/main/java/com/mealmentor/enterprise/dto/MealItem.java
@@ -2,12 +2,21 @@ package com.mealmentor.enterprise.dto;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public @Data
 class MealItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer recipeId;
     private String mealtime;
     private String day;
-    private Recipe recipe;
-    private Integer recipeId;
+
 
     public String getMealItemId() {
         String mealItemId = "";
