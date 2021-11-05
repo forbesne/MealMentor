ff2f01631320bbdb5f10b9513e0aa6b36e6839b8
sukhbirsekhon <sekhonss@mail.uc.edu>
2021-11-05 14:08:43 -0400

    Unused import removed

--- src/main/java/com/mealmentor/enterprise/MealMentorController.java
+++ src/main/java/com/mealmentor/enterprise/MealMentorController.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.mealmentor.enterprise.dto.Recipe;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
