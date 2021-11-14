2e798434f99147d87062dc03a6536dd1a6a97095
ThapaArchana <thapaan@mail.uc.edu>
2021-11-14 15:29:14 -0500

    Initialized logger

--- src/main/java/com/mealmentor/enterprise/MealMentorController.java
+++ src/main/java/com/mealmentor/enterprise/MealMentorController.java
@@ -3,6 +3,8 @@ package com.mealmentor.enterprise;
 import com.mealmentor.enterprise.dto.MealItem;
 import com.mealmentor.enterprise.dto.RecipeLabelValue;
 import com.mealmentor.enterprise.service.IMealPlanService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -26,6 +28,8 @@ public class MealMentorController {
 
     @Autowired
     IMealPlanService mealPlanService;
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("/")
     public String index() {
